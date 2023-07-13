52d93aa4ca05e36939ff5fe4c7d7715cf16eec34
giriprakash <85009018+spider-251@users.noreply.github.com>
2023-07-13 12:44:09 +0530

    Add files via upload

---
+++ docgen_project/urls.py
@@ -0,0 +1,24 @@
+"""docgen_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from docgen_app.views import code_editor, pdf_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('code-editor/', code_editor, name='code_editor'),
+    path('pdf_view/', pdf_view, name='pdf_view')
+]
